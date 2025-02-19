69b335f6ea7fd72b226970361a2c1c929b6d8285
ToninoDB <98458711+ToninoDB@users.noreply.github.com>
2025-02-19 15:47:04 +0100

    Gioco indovina numero segreto
    Genera intero compreso tra 1 e 100 e chiedere all'utente di tentare di indovinare, fornendo dei suggerimento "Troppo alto" o "Troppo basso".
    Itera fin quando non si indovina il numero generato casualmente.

---
+++ EsercizioTre.java
@@ -0,0 +1,44 @@
+package EserciziPomeriggioWhile;
+
+import java.util.Random;
+import java.util.Scanner;
+
+//Gioco indovina numero segreto
+//Genera intero compreso tra 1 e 100 e chiedere all'utente di tentare di indovinare,
+//fornendo dei suggerimento "Troppo alto" o "Troppo basso".
+
+public class EsercizioTre {
+    public static void main(String[] args) {
+        // Apertura scanner
+        Scanner input = new Scanner(System.in);
+        // Dichiarazione variabile della classe Random per il numero casuale
+        Random random = new Random();
+
+        // Dichiarazione e inizializzazione del numero casuale generato
+        int numeroDaIndovinare = random.nextInt(100) + 1;
+
+        // Inizializzazione ciclo while booleano
+        while (true) {
+
+            int numeroUtente;
+
+            do {
+                System.out.print("Prova ad indovinare il numero intero tra 1 e 100: ");
+                numeroUtente = input.nextInt();
+            } while (numeroUtente <= 0);
+
+            // Controllo se il numero è stato indovinato
+            if (numeroUtente < numeroDaIndovinare)
+                System.out.println("Troppo basso!");
+            else if (numeroUtente > numeroDaIndovinare)
+                System.out.println("Troppo alto!");
+            else {
+                System.out.println("Complimenti hai indovinato!");
+                break;
+            }
+        }
+
+        // Chiusura scanner
+        input.close();
+    }
+}
